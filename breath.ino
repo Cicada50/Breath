980d352dd279ad1372e49f33e866e62503f8d995
Cicada50 <54494776+Cicada50@users.noreply.github.com>
2020-12-08 21:30:11 +0800

    Add files via upload
    Breath_useLib_17.zip 为定义的呼吸灯的库文件
    DHTtester.ino为调用上述库文件DHT11温湿度采集程序
    breath.ino调用上述库文件呼吸灯程序

---
+++ breath.ino
@@ -0,0 +1,14 @@
+#include <breath.h>
+
+
+
+Breath pin(13);
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pin.breath(true,8);
+}
